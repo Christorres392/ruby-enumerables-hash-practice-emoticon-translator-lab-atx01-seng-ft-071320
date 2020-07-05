56b5d45a178edf146e2a240c867619093d7ec19c
Christopher Torres <christorres09@gmail.com>
2020-07-05 07:36:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,17 +16,9 @@ end
 return_hash
 end
 =end
-def load_library(file_path)
-  emoticons = YAML.load_file(file_path)
-  new_hash = {}
+def load_library(file)
+  hash = {}
 
-  emoticons.each do |key,value|
-    new_hash[key] = {}
-    new_hash[key][:english] = value[0]
-    new_hash[key][:japanese] = value[1]
-  end
-
-  new_hash
 end
 
 def get_japanese_emoticon
