9ac2104ffc27e5f030841627a644e1090ba4b7b4
Christopher Torres <christorres09@gmail.com>
2020-07-05 05:55:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,8 @@ require "pry"
 def load_library(file)
   # code goes here
 emoticons = YAML.load(file)
-emoticon_lib ={}
-binding.pry
+emoticon_lib = {meaning: {}, emoticon: {}}
+
 end
 
 def get_japanese_emoticon
